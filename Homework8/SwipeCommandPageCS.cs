23c337580c1feb0374b3ec04654e42300542fc04
jjbman52 <jjbman52@gmail.com>
2019-10-15 20:48:29 -0500

    Initial check-in of module Homework8

---
+++ Homework8/SwipeCommandPageCS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Homework8
+{
+	public class SwipeCommandPageCS : ContentPage
+	{
+		public SwipeCommandPageCS()
+		{
+			var boxView = new BoxView { Color = Color.Teal, WidthRequest = 300, HeightRequest = 300, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.CenterAndExpand };
+			var label = new Label();
+			label.SetBinding(Label.TextProperty, "SwipeDirection");
+
+			var leftSwipeGesture = new SwipeGestureRecognizer { Direction = SwipeDirection.Left, CommandParameter = "Left" };
+			leftSwipeGesture.SetBinding(SwipeGestureRecognizer.CommandProperty, "SwipeCommand");
+
+			var rightSwipeGesture = new SwipeGestureRecognizer { Direction = SwipeDirection.Right, CommandParameter = "Right" };
+			rightSwipeGesture.SetBinding(SwipeGestureRecognizer.CommandProperty, "SwipeCommand");
+
+			boxView.GestureRecognizers.Add(leftSwipeGesture);
+			boxView.GestureRecognizers.Add(rightSwipeGesture);
+
+			Content = new StackLayout
+			{
+				Margin = new Thickness(20),
+				Children = {
+					new Label { Margin = new Thickness(0,10), Text = "Swipe inside the box with a single finger." },
+					boxView,
+					label
+				}
+			};
+			BindingContext = new SwipeCommandPageViewModel();
+		}
+	}
+}
---
+++ Homework8/SwipeCommandPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Homework8
+{
+	public class SwipeCommandPageViewModel : INotifyPropertyChanged
+	{
+        public string[] imgs = {"Chick1","Chick2","Chick3","Chick4","Chick5","Duck1","Duck2","Duck3","Duck4","Duck5"};
+
+		public ICommand SwipeCommand => new Command<string>(Swipe);
+
+		public string img { get; private set; }
+
+        public int count = 0;
+
+		public SwipeCommandPageViewModel()
+		{
+			img = imgs[new Random().Next(0,9)];
+		}
+
+		void Swipe(string value)
+		{
+            if (value == "right" && img.TrimEnd(img[img.Length - 1]) == "Chick" || value == "right" && img.TrimEnd(img[img.Length - 1]) == "Duck")
+            {
+                img = imgs[new Random().Next(0, 9)];
+                OnPropertyChanged("img");
+                count++;
+            }
+            else
+            {
+
+            }
+        }
+
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+	}
+}
+
